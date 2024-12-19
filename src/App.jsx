b07420730a84f70ead903ceaa71d2ddc2c56c282
prithvish774848474 <prithvish.freelance@gmail.com>
2024-12-19 23:30:02 +0530

    feat:Added new features related to Modals and Footer, and added comments in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { NameInputModal } from "../components/Modal.jsx";
-import { MedicationDetailsFormSubmissionModal } from "../components/Modal.jsx";
+import { Footer } from "../components/Footer.jsx";
+import { NameInputModal } from "../components/Modals.jsx";
+import { MedicationDetailsFormSubmissionModal } from "../components/Modals.jsx";
+import { UserGuideModal } from "../components/Modals.jsx";
+import { DeleteLocalStorageData } from "../components/Modals.jsx";
 import { NavigationBar } from "../components/Navigation.jsx";
 import { Dashboard } from "../components/Dashboard.jsx";
 import { SideBar } from "../components/SideBar.jsx";
@@ -9,21 +12,31 @@ import { CoverPage } from "../components/CoverPage.jsx";
 import { useState, useEffect } from "react";
 
 export default function App() {
+  // The 'componentIndex' State Variable is mapped to certain main components
+  // It is used for dynamic display of main component
+  // It is changed when the Navigation Buttons are clicked
   const [componentIndex, setComponentIndex] = useState(-1);
+
+  // The 'userName' State Variable is used to capture the user's name
+  // The User Name is displayed across all the main components
+  // The 'userName' is stored in the Local Storage and fetched whenever required
   const [userName, setUserName] = useState(() => {
     const localStorageData = window.localStorage.getItem("medicine-user-name");
     return localStorageData ? localStorageData : "";
   });
+
+  // A Side Effect runs whenever the 'userName' changes
+  // Side Effect stored the 'userName' in the Local Storage
   useEffect(() => {
     window.localStorage.setItem("medicine-user-name", userName);
   }, [userName]);
-  const [inputModalDisplay, setInputModalDisplay] = useState(true);
-  useEffect(() => {
-    const localStorageData = window.localStorage.getItem("medicine-user-name");
-    localStorageData && setInputModalDisplay(false);
-  }, []);
+
+  // The 'displayComponent' stores the main component that is to be displayed dynamically
+  // By default, it is set to show the Cover Page when the App mounts
   const [displayComponent, setDisplayComponent] = useState(<CoverPage />);
 
+  // This Medication Card Details data is used initially as an example or a dummy data
+  // This data is just to make users familiar with the App
   const initialMedicationData = [
     {
       name: "Oxymetazoline Hydrochloride",
@@ -57,7 +70,9 @@ export default function App() {
     },
   ];
 
-  // Retrieving Data from Local Storage
+  // The 'medicineCardDetails' stores the actual data of all the medicines
+  // It is an Array of Objects, where each Object stores one Medicine's Detail
+  // This Array is stored in the Local Storage and retieved when required
   const [medicineCardDetails, setMedicineCardDetails] = useState(() => {
     const localStorageData = window.localStorage.getItem("medication-data");
     return localStorageData
@@ -65,7 +80,8 @@ export default function App() {
       : initialMedicationData;
   });
 
-  // Updating Data in the Local Storage
+  // A Side Effect runs whenever the 'medicineCardDetails' State Variable changes
+  // Side Effect stores the Medicine Details in the Local Storage
   useEffect(() => {
     window.localStorage.setItem(
       "medication-data",
@@ -73,7 +89,9 @@ export default function App() {
     );
   }, [medicineCardDetails]);
 
-  // Conditionally Rendering the Components
+  // A Side Effect runs whenever the 'componentIndex' changes
+  // Navigation Button click -> componentIndex changes
+  // Side Effect updates the 'displayComponent' to render one of the main component
   useEffect(() => {
     switch (componentIndex) {
       case 0:
@@ -106,28 +124,65 @@ export default function App() {
     }
   }, [componentIndex]);
 
+  // The 'showInputModal' either shows or hides the Modal to intake username
+  // The Modal is only shown if there is no username in the Local Storage
+  // The Modal when shown will take the username and update the 'userName' State Variable
+  // The Input Modal is displayed by default in every render
+  const [showInputModal, setShowInputModal] = useState(true);
+
+  // A Side Effect runs whenever the application mounts
+  // This Side Effect ensures that the Modal is only displayed if the Local Storage is empty
+  useEffect(() => {
+    const localStorageData = window.localStorage.getItem("medicine-user-name");
+    localStorageData && setShowInputModal(false);
+  }, []);
+
+  // This invokes the Modal for successful submission of Medication details
+  // This waives out the Modal after 2 seconds
   const [showSubmissionModal, setShowSubmissionModal] = useState(false);
+
+  // The User Guide Modal is displayed or erased depending on user's action
+  const [showUserGuideModal, setShowUserGuideModal] = useState(false);
+
+  // The Local Storage Deletion Modal is either shown or hidden
+  // The Local Storage Data related to the App will be erased if "Delete" button is clicked
+  // The Local Storage Data will not be erased if "Cancel" button is clicked
+  const [showDeleteDataModal, setShowDeleteDataModal] = useState(false);
 
   return (
     <section>
-      {inputModalDisplay && (
+      {showInputModal && (
         <NameInputModal
           updateUserName={setUserName}
-          updateInputModalDisplay={setInputModalDisplay}
+          updateShowInputModal={setShowInputModal}
         />
       )}
       {showSubmissionModal && <MedicationDetailsFormSubmissionModal />}
-      <section className="md:flex md:flex-row md:items-start md:justify-stretch">
+      {showUserGuideModal && (
+        <UserGuideModal updateShowUserGuideModal={setShowUserGuideModal} />
+      )}
+      {showDeleteDataModal && (
+        <DeleteLocalStorageData
+          updateShowDeleteDataModal={setShowDeleteDataModal}
+          numberOfChecklist={medicineCardDetails.length}
+        />
+      )}
+      <section className="md:flex md:flex-row md:items-start md:justify-stretch
+      bg-white md:min-h-[100vh]">
         <section
           className="p-2 flex flex-col gap-2 items-stretch justify-stretch
             md:basis-[30%] md:shrink-0 md:grow-0
             border-2 border-solid fancy-border md:border-none"
         >
           <NavigationBar updateComponentIndex={setComponentIndex} />
-          <SideBar />
+          <SideBar
+            updateShowUserGuideModal={setShowUserGuideModal}
+            updateShowDeleteDataModal={setShowDeleteDataModal}
+          />
         </section>
         {displayComponent}
       </section>
+      <Footer />
     </section>
   );
 }
